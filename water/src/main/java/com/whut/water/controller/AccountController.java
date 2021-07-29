d7d0f503ab8a2bab1e5040f1950b56f71b74ecc9
githubhuangyaohui <persistenthuang@163.com>
2021-07-29 15:12:28 +0800

    添加md

--- water/src/main/java/com/whut/water/controller/AccountController.java
+++ water/src/main/java/com/whut/water/controller/AccountController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpSession;
 
 @Slf4j
